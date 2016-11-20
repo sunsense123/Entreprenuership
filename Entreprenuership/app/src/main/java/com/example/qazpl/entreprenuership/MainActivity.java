db33afb3d3b1b38acaa7cfaf449d61aee807dc9b
Jeonghyun <qazplm95147@gmail.com>
2016-11-21 01:28:33 +0900

    Inital version

---
+++ Entreprenuership/app/src/main/java/com/example/qazpl/entreprenuership/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.qazpl.entreprenuership;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent mintent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(mintent);
+    }
+}
