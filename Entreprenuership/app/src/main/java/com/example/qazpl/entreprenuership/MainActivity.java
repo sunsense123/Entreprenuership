24ad6c1af0cbbf4c2b7e51cd847d65e8a45ac8e4
민태원 <k4rr@protonmail.com>
2016-11-29 00:35:14 +0900

    add Downstream messaging example

--- Entreprenuership/app/src/main/java/com/example/qazpl/entreprenuership/MainActivity.java
+++ Entreprenuership/app/src/main/java/com/example/qazpl/entreprenuership/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.qazpl.entreprenuership;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Intent mintent = new Intent(MainActivity.this,SecondActivity.class);
-        //startActivity(mintent);
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+        Log.d("Firebase","Token: "+token);
     }
 }
